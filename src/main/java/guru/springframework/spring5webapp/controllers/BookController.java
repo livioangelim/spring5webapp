d9640207334edb32b64adf6c8db13a4a687f2856
LIVIO ANGELIM MORORO <lange70@wgu.edu>
2024-08-15 17:51:18 -0400

    Created and configured BookController.java

---
+++ src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
@@ -0,0 +1,29 @@
+package guru.springframework.spring5webapp.controllers;
+
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller //1.annotation for controllers
+public class BookController {
+
+
+    private final BookRepository bookRepository; //3.declare field
+
+    //3.constructor to inject the bookRepository using the private field
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books") //2.annotation for request mapping with method referencing to books page
+    public String getBooks(Model model) {
+    //2.the Model model above is connecting to Model POJO required by the view
+
+        model.addAttribute("books", bookRepository.findAll());
+        //4.when Spring receives the request to the /books url it will execute the books method providing the method to model
+        //4.The model object will add the attribute called books, which will return to the viewer the info required
+
+        return "books"; //2 declaration of the getBooks method returning the attribute model books
+    }
+}
